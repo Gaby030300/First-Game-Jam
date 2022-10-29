5f920ad225a980f79434337f70d9fc93ebb477de
treu <luis.medina@utp.edu.co>
2022-10-29 18:55:29 -0500

    mision3

---
+++ Assets/Scripts/Line.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Line : MonoBehaviour
+{
+    [SerializeField] private LineRenderer _renderer;
+    [SerializeField] private EdgeCollider2D _collider;
+
+    private readonly List<Vector2> _points = new List<Vector2>();
+    private int i = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _collider.transform.position -= transform.position;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void SetPosition(Vector2 pos)
+    {
+        if (!CanAppend(pos)) return;
+        //Debug.Log(_renderer.GetPosition(0));
+        //Debug.Log(Vector2.Distance(_renderer.GetPosition(0), _renderer.GetPosition(_renderer.positionCount - 1))   );
+        _points.Add(pos);
+        _renderer.positionCount++;
+        _renderer.SetPosition(_renderer.positionCount-1, pos);
+
+        _collider.points = _points.ToArray();
+
+        if(Vector2.Distance(_renderer.GetPosition(0), _renderer.GetPosition(_renderer.positionCount - 1)) >0.1 && Vector2.Distance(_renderer.GetPosition(0), _renderer.GetPosition(_renderer.positionCount - 1)) <0.2)
+        {
+            i++;
+            if (i == 2)
+            {
+                Debug.Log(" Pasar siguiente nivel");
+            }
+            
+        }
+        
+
+
+
+
+    }
+
+    private bool CanAppend(Vector2 pos)
+    {
+        if (_renderer.positionCount == 0) return true;
+
+        
+
+        return Vector2.Distance(_renderer.GetPosition(_renderer.positionCount - 1), pos) > DrawManager.RESOLUTION;
+    }
+}
---
+++ Assets/Scripts/Wall.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+    void OnMouseOver()
+    {
+        print(gameObject.name); //If o condicion de reiniciar el nivel
+    }
+
+
+}
